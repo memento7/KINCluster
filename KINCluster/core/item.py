cbe02d02fd03fbc28ea07b0c5baa8584cfb7a667
MaybeS <maytryark@gmail.com>
2017-04-10 15:54:29 +0900

    Update core.item type hint

--- KINCluster/core/item.py
+++ KINCluster/core/item.py
@@ -1,21 +1,23 @@
 import settings
 
+from typing import List, Any
+
 class Item:
     def __init__(self, **kwargs):
         self._element = {}
         for k, v in kwargs.items():
             self._element[k] = v 
 
-    def __str__(self):
+    def __str__(self) -> str:
         """represent text to clustering
             :apply n-gram or stemize if you need
         """
-        return " ".join(self.element)
+        return " ".join(map(str, self.element))
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """represent vocabs in item
         """
-        return " ".join(self.element)
+        return " ".join(map(str, self.element))
 
     def __eq__(self, other):
         if isinstance(other, self.__class__):
@@ -23,9 +25,9 @@ class Item:
         return False 
 
     @property
-    def element(self):
+    def element(self) -> List[Any]:
         return [self._element[k] for k in self.element_keys]
 
     @property
-    def element_keys(self):
+    def element_keys(self) -> List[str]:
         return sorted(self._element.keys())
