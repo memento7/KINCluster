e50ce06f2831862e2d2e75031ac4483c568e0476
MaybeS <maytryark@gmail.com>
2017-04-25 12:30:39 +0900

    Update test

--- tests/test_KINCluster.py
+++ tests/test_KINCluster.py
@@ -49,53 +49,6 @@ def test_app1():
         assert isinstance(extracted.topic, Item)
         pipeline.dress_item(extracted, items)
 
-# Test2
-import pymysql
-SERVER_RDB = '175.207.13.225'
-class PipelineServer(Pipeline):
-    def __init__(self, keyword):
-        self.keyword = keyword
-    def capture_item(self):
-        conn = pymysql.connect(host=SERVER_RDB,
-                           user='memento',
-                           password='memento@0x100_',
-                           db='memento',
-                           charset='utf8')
-        cur = conn.cursor()
-
-        columns = ['keyword', 'title', 'content', 'published_time']
-
-        sql = "SELECT " + ",".join(columns) + " FROM articles where keyword like \'" + self.keyword  +"\' limit 1000"
-
-        result = cur.execute(sql)
-
-        for keyword, title, content, ptime in cur:
-            yield Item(title=title,content=content,keyword=keyword,date=ptime)
-
-        cur.close()
-        conn.close()
-
-def test_app2():
-    """ Testing for cluster, using test data
-    """
-    cluster = Cluster(epoch=32, tokenizer="tokenize")
-    pipe = PipelineServer('김태희')
-
-    for item in pipe.capture_item():
-        cluster.put_item(item)
-    cluster.cluster()
-
-    extractor = Extractor(cluster)
-    for idx, dump in enumerate(cluster.dumps):
-        items, vectors = map(list, zip(*dump))
-
-        extracted = extractor.dump(idx)
-
-        # assert isinstance(extracted, Item)
-
-        pipe.dress_item(extracted, items)
-    print (cluster.distribution)
-
 # Test3
 import pandas as pd
 from nltk import ngrams
