0a6a6b59d9f022a829591669d46fc935abb2ae12
MaybeS <maytryark@gmail.com>
2017-04-28 19:53:02 +0900

    Release 0.1.5.1

--- KINCluster/__init__.py
+++ KINCluster/__init__.py
@@ -1,13 +1,29 @@
 """
 KINCluster is clustering like KIN.
 
-release note: version 0.1.4.5
+
+
+release note: 
+- version 0.1.5.1
+    rebase module, just import KINCluster
+    simplify extractor module
+
+- version 0.1.4.5
     First release
 """
 
-__version__ = '0.1.4.5'
+__version__ = '0.1.5.1'
 
-__all__ = ['core', 'lib']
+__all__ = ['KINCluster', 
+'Cluster', 'Extractor', 'Item', 'Pipeline', 
+'tokenizer', 'stopwords']
 
-from KINCluster import core
-from KINCluster import lib
+from KINCluster.KINCluster import KINCluster
+
+from KINCluster.core.cluster import Cluster 
+from KINCluster.core.extractor import Extractor 
+from KINCluster.core.item import Item 
+from KINCluster.core.pipeline import Pipeline 
+
+from KINCluster.lib.tokenizer import tokenizer
+from KINCluster.lib.stopwords import stopwords
